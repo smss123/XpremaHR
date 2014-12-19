6faf6fab9bf469328ce31ea6efec56541cdd5d43
sharfmhd <pko@hotmail.com>
2014-12-19 23:36:52 +0300

    Vacations table added

---
+++ XpremaHR/Database1/tbl_Vacations.sql
@@ -0,0 +1,20 @@
+USE [XpremaHR]
+GO
+
+/****** Object:  Table [EmpInfo].[Vacations]    Script Date: 12/19/2014 23:34:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [EmpInfo].[Vacations](
+	[EmpNumber] [int] NULL,
+	[Start_Date] [date] NULL,
+	[End_date] [date] NULL,
+	[Vacation_type] [nchar](200) NULL
+) ON [PRIMARY]
+
+GO
+
+

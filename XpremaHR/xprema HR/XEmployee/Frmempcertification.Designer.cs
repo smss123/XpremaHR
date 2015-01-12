532174ba2a2121c68909ac7764a7c60fcfd3a3db
sharfmhd <pko@hotmail.com>
2015-01-12 21:01:27 +0300

    frm certification

---
+++ XpremaHR/xprema HR/XEmployee/Frmempcertification.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace xprema_HR.XEmployee
+{
+    partial class Frmempcertification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radGroupBox1 = new Telerik.WinControls.UI.RadGroupBox();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.txtqualific = new Telerik.WinControls.UI.RadTextBox();
+            this.txtcareer = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel4 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel3 = new Telerik.WinControls.UI.RadLabel();
+            this.txtempID = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox1)).BeginInit();
+            this.radGroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtqualific)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtcareer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtempID)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radGroupBox1
+            // 
+            this.radGroupBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGroupBox1.Controls.Add(this.radButton1);
+            this.radGroupBox1.Controls.Add(this.txtqualific);
+            this.radGroupBox1.Controls.Add(this.txtcareer);
+            this.radGroupBox1.Controls.Add(this.radLabel4);
+            this.radGroupBox1.Controls.Add(this.radLabel3);
+            this.radGroupBox1.Controls.Add(this.txtempID);
+            this.radGroupBox1.Controls.Add(this.radLabel1);
+            this.radGroupBox1.FooterTextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.radGroupBox1.HeaderText = "Employee Certifcations";
+            this.radGroupBox1.Location = new System.Drawing.Point(12, 12);
+            this.radGroupBox1.Name = "radGroupBox1";
+            this.radGroupBox1.Size = new System.Drawing.Size(357, 195);
+            this.radGroupBox1.TabIndex = 1;
+            this.radGroupBox1.Text = "Employee Certifcations";
+            this.radGroupBox1.ThemeName = "ControlDefault";
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(99, 146);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(109, 37);
+            this.radButton1.TabIndex = 21;
+            this.radButton1.Text = "Add";
+            this.radButton1.ThemeName = "ControlDefault";
+            // 
+            // txtqualific
+            // 
+            this.txtqualific.Location = new System.Drawing.Point(99, 106);
+            this.txtqualific.Name = "txtqualific";
+            this.txtqualific.Size = new System.Drawing.Size(209, 20);
+            this.txtqualific.TabIndex = 11;
+            // 
+            // txtcareer
+            // 
+            this.txtcareer.Location = new System.Drawing.Point(99, 71);
+            this.txtcareer.Name = "txtcareer";
+            this.txtcareer.Size = new System.Drawing.Size(209, 20);
+            this.txtcareer.TabIndex = 11;
+            // 
+            // radLabel4
+            // 
+            this.radLabel4.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.radLabel4.Location = new System.Drawing.Point(5, 106);
+            this.radLabel4.Name = "radLabel4";
+            this.radLabel4.Size = new System.Drawing.Size(75, 18);
+            this.radLabel4.TabIndex = 10;
+            this.radLabel4.Text = "Qualification :";
+            // 
+            // radLabel3
+            // 
+            this.radLabel3.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.radLabel3.Location = new System.Drawing.Point(5, 71);
+            this.radLabel3.Name = "radLabel3";
+            this.radLabel3.Size = new System.Drawing.Size(44, 18);
+            this.radLabel3.TabIndex = 10;
+            this.radLabel3.Text = "Career :";
+            // 
+            // txtempID
+            // 
+            this.txtempID.Location = new System.Drawing.Point(99, 33);
+            this.txtempID.Name = "txtempID";
+            this.txtempID.Size = new System.Drawing.Size(209, 20);
+            this.txtempID.TabIndex = 9;
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.radLabel1.Location = new System.Drawing.Point(5, 33);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(45, 18);
+            this.radLabel1.TabIndex = 7;
+            this.radLabel1.Text = "EMP ID:";
+            // 
+            // Frmempcertification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(383, 218);
+            this.Controls.Add(this.radGroupBox1);
+            this.Name = "Frmempcertification";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "Employee Certifcations";
+            this.ThemeName = "VisualStudio2012Light";
+            this.Load += new System.EventHandler(this.Frmempcertification_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox1)).EndInit();
+            this.radGroupBox1.ResumeLayout(false);
+            this.radGroupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtqualific)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtcareer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtempID)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadGroupBox radGroupBox1;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadTextBox txtqualific;
+        private Telerik.WinControls.UI.RadTextBox txtcareer;
+        private Telerik.WinControls.UI.RadLabel radLabel4;
+        private Telerik.WinControls.UI.RadLabel radLabel3;
+        private Telerik.WinControls.UI.RadTextBox txtempID;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+    }
+}
---
+++ XpremaHR/xprema HR/XEmployee/Frmempcertification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace xprema_HR.XEmployee
+{
+    public partial class Frmempcertification : Telerik.WinControls.UI.RadForm
+    {
+        public Frmempcertification()
+        {
+            InitializeComponent();
+        }
+
+        private void Frmempcertification_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

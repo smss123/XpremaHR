f3cd7fc4b39140c4b3c7aea7d36691ace580a6cb
sharfmhd <pko@hotmail.com>
2015-02-22 21:28:02 +0300

    FRM AccountDaily Added

---
+++ XpremaHR/xprema HR/XAccounts/FrmAccountTree.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace xprema_HR.XAccounts
+{
+    partial class FrmAccountTree
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.office2013LightTheme1 = new Telerik.WinControls.Themes.Office2013LightTheme();
+            this.radGroupBox1 = new Telerik.WinControls.UI.RadGroupBox();
+            this.radGroupBox2 = new Telerik.WinControls.UI.RadGroupBox();
+            this.radGroupBox3 = new Telerik.WinControls.UI.RadGroupBox();
+            this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+            this.radTreeView1 = new Telerik.WinControls.UI.RadTreeView();
+            this.BtnCollapse = new System.Windows.Forms.Button();
+            this.BtnExpand = new System.Windows.Forms.Button();
+            this.txtcridet = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtAccountName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox1)).BeginInit();
+            this.radGroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox2)).BeginInit();
+            this.radGroupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox3)).BeginInit();
+            this.radGroupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTreeView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radGroupBox1
+            // 
+            this.radGroupBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGroupBox1.Controls.Add(this.radGridView1);
+            this.radGroupBox1.HeaderText = "Account Details";
+            this.radGroupBox1.Location = new System.Drawing.Point(359, 12);
+            this.radGroupBox1.Name = "radGroupBox1";
+            this.radGroupBox1.Size = new System.Drawing.Size(570, 420);
+            this.radGroupBox1.TabIndex = 0;
+            this.radGroupBox1.Text = "Account Details";
+            // 
+            // radGroupBox2
+            // 
+            this.radGroupBox2.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGroupBox2.Controls.Add(this.BtnCollapse);
+            this.radGroupBox2.Controls.Add(this.BtnExpand);
+            this.radGroupBox2.Controls.Add(this.radTreeView1);
+            this.radGroupBox2.HeaderText = "__";
+            this.radGroupBox2.Location = new System.Drawing.Point(12, 12);
+            this.radGroupBox2.Name = "radGroupBox2";
+            this.radGroupBox2.Size = new System.Drawing.Size(341, 306);
+            this.radGroupBox2.TabIndex = 1;
+            this.radGroupBox2.Text = "__";
+            // 
+            // radGroupBox3
+            // 
+            this.radGroupBox3.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGroupBox3.Controls.Add(this.txtcridet);
+            this.radGroupBox3.Controls.Add(this.txtDescription);
+            this.radGroupBox3.Controls.Add(this.txtAccountName);
+            this.radGroupBox3.Controls.Add(this.label3);
+            this.radGroupBox3.Controls.Add(this.label2);
+            this.radGroupBox3.Controls.Add(this.label1);
+            this.radGroupBox3.HeaderText = "Details";
+            this.radGroupBox3.Location = new System.Drawing.Point(12, 329);
+            this.radGroupBox3.Name = "radGroupBox3";
+            this.radGroupBox3.Size = new System.Drawing.Size(341, 105);
+            this.radGroupBox3.TabIndex = 2;
+            this.radGroupBox3.Text = "Details";
+            // 
+            // radGridView1
+            // 
+            this.radGridView1.Location = new System.Drawing.Point(6, 22);
+            this.radGridView1.Name = "radGridView1";
+            this.radGridView1.Size = new System.Drawing.Size(564, 399);
+            this.radGridView1.TabIndex = 0;
+            this.radGridView1.Text = "radGridView1";
+            // 
+            // radTreeView1
+            // 
+            this.radTreeView1.Location = new System.Drawing.Point(5, 22);
+            this.radTreeView1.Name = "radTreeView1";
+            this.radTreeView1.Size = new System.Drawing.Size(313, 245);
+            this.radTreeView1.SpacingBetweenNodes = -1;
+            this.radTreeView1.TabIndex = 0;
+            this.radTreeView1.Text = "radTreeView1";
+            // 
+            // BtnCollapse
+            // 
+            this.BtnCollapse.Location = new System.Drawing.Point(5, 271);
+            this.BtnCollapse.Name = "BtnCollapse";
+            this.BtnCollapse.Size = new System.Drawing.Size(125, 30);
+            this.BtnCollapse.TabIndex = 4;
+            this.BtnCollapse.Text = "Collapse All";
+            this.BtnCollapse.UseVisualStyleBackColor = true;
+            // 
+            // BtnExpand
+            // 
+            this.BtnExpand.Location = new System.Drawing.Point(180, 271);
+            this.BtnExpand.Name = "BtnExpand";
+            this.BtnExpand.Size = new System.Drawing.Size(138, 30);
+            this.BtnExpand.TabIndex = 3;
+            this.BtnExpand.Text = "Expaned All";
+            this.BtnExpand.UseVisualStyleBackColor = true;
+            // 
+            // txtcridet
+            // 
+            this.txtcridet.Location = new System.Drawing.Point(101, 68);
+            this.txtcridet.Name = "txtcridet";
+            this.txtcridet.Size = new System.Drawing.Size(227, 20);
+            this.txtcridet.TabIndex = 11;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(101, 42);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(227, 20);
+            this.txtDescription.TabIndex = 10;
+            // 
+            // txtAccountName
+            // 
+            this.txtAccountName.Location = new System.Drawing.Point(101, 16);
+            this.txtAccountName.Name = "txtAccountName";
+            this.txtAccountName.Size = new System.Drawing.Size(227, 20);
+            this.txtAccountName.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Cridet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Account Name :";
+            // 
+            // FrmAccountTree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 446);
+            this.Controls.Add(this.radGroupBox3);
+            this.Controls.Add(this.radGroupBox2);
+            this.Controls.Add(this.radGroupBox1);
+            this.Name = "FrmAccountTree";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "FrmAccountTree";
+            this.ThemeName = "Office2013Light";
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox1)).EndInit();
+            this.radGroupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox2)).EndInit();
+            this.radGroupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBox3)).EndInit();
+            this.radGroupBox3.ResumeLayout(false);
+            this.radGroupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTreeView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.Themes.Office2013LightTheme office2013LightTheme1;
+        private Telerik.WinControls.UI.RadGroupBox radGroupBox1;
+        private Telerik.WinControls.UI.RadGroupBox radGroupBox2;
+        private Telerik.WinControls.UI.RadGroupBox radGroupBox3;
+        private Telerik.WinControls.UI.RadGridView radGridView1;
+        private Telerik.WinControls.UI.RadTreeView radTreeView1;
+        private System.Windows.Forms.Button BtnCollapse;
+        private System.Windows.Forms.Button BtnExpand;
+        private System.Windows.Forms.TextBox txtcridet;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtAccountName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}

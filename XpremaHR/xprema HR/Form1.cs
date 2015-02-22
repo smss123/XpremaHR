1af2e619e0dfcff0420f10fa07e109098973d845
sharfmhd <pko@hotmail.com>
2015-02-22 19:28:57 +0300

    بعمل في تست للكوميت

--- XpremaHR/xprema HR/Form1.cs
+++ XpremaHR/xprema HR/Form1.cs
@@ -16,5 +16,10 @@ namespace xprema_HR
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //test commit :)
+        }
     }
 }
